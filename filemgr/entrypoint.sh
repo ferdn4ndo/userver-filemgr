81926cf2665ecd5f9f554360e3097473235635cc
Fernando Constantino <const.fernando@gmail.com>
2023-03-16 04:27:44 -0300

    Changed API service port from 9905 to 5000

--- filemgr/entrypoint.sh
+++ filemgr/entrypoint.sh
@@ -4,9 +4,9 @@ if [ "${ENV_MODE}" == "prod" ]
 then
   echo "Starting in production mode..."
 
-  gunicorn --access-logfile /code/logs/access.log --worker-tmp-dir /dev/shm --workers ${GUVICORN_WORKERS} --bind 0.0.0.0:9905 filemgr.wsgi
+  gunicorn --access-logfile /code/logs/access.log --worker-tmp-dir /dev/shm --workers ${GUVICORN_WORKERS} --bind 0.0.0.0:5000 filemgr.wsgi
 else
   echo "Starting in development mode..."
 
-  python manage.py runserver 0.0.0.0:9905
+  python manage.py runserver 0.0.0.0:5000
 fi
